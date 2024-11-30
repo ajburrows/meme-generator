f4da65be6e5e7a76c149b1b334cbf1f49fb6f048
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 12:42:40 -0500

    move meme data to a state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText:"Walk into Mordor",
+        imgUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        placeholder={meme.topText}
                         name="topText"
                     />
                 </label>
@@ -13,14 +21,14 @@ export default function Main() {
                 <label>Bottom Text
                     <input
                         type="text"
-                        placeholder="Walk into Mordor"
+                        placeholder={meme.bottomText}
                         name="bottomText"
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
+                <img src={meme.imgUrl} />
                 <span className="top">One does not simply</span>
                 <span className="bottom">Walk into Mordor</span>
             </div>
